976bbc42457f1f460c2fa6ce7f62a69b4786add5
Enrico <enrico.vanghetti@scout24.com>
2019-11-14 01:32:29 +0100

    Fix tests

--- test/client.spec.ts
+++ test/client.spec.ts
@@ -12,19 +12,16 @@ const userInBucket22CultureIT = {
     culture: 'it-IT',
     uuid: '88248687-6dce-4759-a5c0-3945eedc2b48',
 } // bucket: 22
-// const userInBucket76CultureIT = {
-//     culture: 'it-IT',
-//     uuid: '721f87e2-cec9-4753-b3bb-d2ebe20dd317',
-// }; // bucket: 76
 
 import mockedTogglestate from './togglestate.fixture.json'
+import { Toggles } from '../src/types/Toggles'
 
 jest.mock('axios', () => {
     return jest.fn().mockImplementation(() => Promise.resolve({ data: mockedTogglestate }))
 })
 
 describe('Toguru Client Usage', () => {
-    it('Basic usage', async () => {
+    test('Basic usage', async () => {
         const client = Client({
             endpoint: 'https://example.com/togglestate',
             refreshIntervalMs: 60 * 1000,
@@ -32,17 +29,28 @@ describe('Toguru Client Usage', () => {
 
         await new Promise((resolve) => setTimeout(resolve, 0))
 
-        expect(client.isToggleEnabled('rolled-out-to-noone', userInBucket22CultureDE)).toBe(false)
+        expect(client.isToggleEnabled({ id: 'not-defined', default: false }, userInBucket22CultureDE)).toBe(false)
         expect(
-            client.isToggleEnabled('rolled-out-to-noone', {
-                ...userInBucket22CultureDE,
-                forcedToggles: { 'rolled-out-to-noone': true },
-            }),
+            client.isToggleEnabled(
+                { id: 'rolled-out-to-noone', default: false },
+                {
+                    ...userInBucket22CultureDE,
+                    forcedToggles: { 'rolled-out-to-noone': true },
+                },
+            ),
         ).toBe(true)
-        expect(client.isToggleEnabled('rolled-out-to-everyone', userInBucketb76CultureDE)).toBe(true)
-        expect(client.isToggleEnabled('rolled-out-only-in-de', userInBucket22CultureIT)).toBe(false)
-        expect(client.isToggleEnabled('rolled-out-to-half-in-de-only', userInBucket22CultureDE)).toBe(true)
-        expect(client.isToggleEnabled('rolled-out-to-half-in-de-only', userInBucket22CultureIT)).toBe(false)
+        expect(client.isToggleEnabled({ id: 'rolled-out-to-everyone', default: false }, userInBucketb76CultureDE)).toBe(
+            true,
+        )
+        expect(client.isToggleEnabled({ id: 'rolled-out-only-in-de', default: false }, userInBucket22CultureIT)).toBe(
+            false,
+        )
+        expect(
+            client.isToggleEnabled({ id: 'rolled-out-to-half-in-de-only', default: false }, userInBucket22CultureDE),
+        ).toBe(true)
+        expect(
+            client.isToggleEnabled({ id: 'rolled-out-to-half-in-de-only', default: false }, userInBucket22CultureIT),
+        ).toBe(false)
     })
 
     it('Advanced features', async () => {
@@ -53,9 +61,11 @@ describe('Toguru Client Usage', () => {
 
         await new Promise((resolve) => setTimeout(resolve, 0))
 
-        expect(client.togglesForService('service2', userInBucket22CultureDE)).toEqual({
-            'rolled-out-to-half-in-de-only': true,
-            'rolled-out-to-noone': false,
-        })
+        expect(client.togglesForService('service2', userInBucket22CultureDE)).toEqual(
+            new Toggles([
+                { id: 'rolled-out-to-half-in-de-only', enabled: true },
+                { id: 'rolled-out-to-noone', enabled: false },
+            ]),
+        )
     })
 })
